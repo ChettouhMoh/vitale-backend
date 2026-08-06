6dae601e0b6f3aafe866a62365a1dda2f0b60f48
Mohamed Chettouh <chettouhmoh80@gmail.com>
2026-08-06 13:34:08 +0100

    barel index for VOs

---
+++ src/doctor/domain/value-objects/index.ts
@@ -0,0 +1,21 @@
+export {
+  VerificationStatus,
+  VerificationStatusValue,
+} from './verification-status.vo';
+export { AccountStatus, AccountStatusValue } from './account-status.vo';
+export {
+  PracticeAffiliation,
+  WorkplaceType,
+} from './practice-affiliation.vo';
+export {
+  WeeklySchedule,
+  DayKey,
+  ScheduleDayStatus,
+  type ScheduleDay,
+} from './weekly-schedule.vo';
+export { ExpertiseList } from './expertise-list.vo';
+export { LanguageList } from './language-list.vo';
+export {
+  EducationHistory,
+  type EducationEntry,
+} from './education-history.vo';
